5bba3932abbada8f4188a709fbc2ed48b3688765
Nick Häcker <nick.athaeck@googlemail.com>
2023-10-20 23:07:33 +0200

    changed functions and logic

--- base/express/index.ts
+++ base/express/index.ts
@@ -32,32 +32,27 @@ export class ExpressRoute {
 }
 
 export abstract class ExpressRoutingAddon {
-    protected routes: ExpressRoute[] = []
     abstract AddRoute(route: ExpressRoute): void;
-    intializeRoutes(app: any): any {
-        for (const route of this.routes) {
-            switch (route.RouteType) {
-                case ExpressRouteType.GET:
-                    app.get(route.Route, route.handleRequest);
-                    break;
-                case ExpressRouteType.POST:
-                    app.post(route.Route, route.handleRequest);
-                    break;
-                case ExpressRouteType.DELETE:
-                    app.delete(route.Route, route.handleRequest);
-                    break;
-                default:
-                    app.put(route.Route, route.handleRequest);
-                    break;
-            }
-        }
-        return app;
-    }
 }
-
+export function AddRoute(app: any, route: ExpressRoute): any {
+    switch (route.RouteType) {
+        case ExpressRouteType.GET:
+            app.get(route.Route, route.handleRequest);
+            break;
+        case ExpressRouteType.POST:
+            app.post(route.Route, route.handleRequest);
+            break;
+        case ExpressRouteType.DELETE:
+            app.delete(route.Route, route.handleRequest);
+            break;
+        default:
+            app.put(route.Route, route.handleRequest);
+            break;
+    }
+    return app;
+}
 export abstract class ExpressRouter extends ExpressRoutingAddon {
     protected app = Router();
-    protected extensions: ExpressRouter[] = [];
     protected path: string;
     protected adapter: string;
 
@@ -80,23 +75,13 @@ export abstract class ExpressRouter extends ExpressRoutingAddon {
         return this.app;
     }
     public AddExtension(extension: ExpressRouter): void {
-        this.extensions.push(extension);
+        this.app.use(extension.path, extension.app);
     }
-    AddRoute(route: ExpressRoute): void{
-        this.routes.push(route);
-    }
-    protected initializeExtensions(app: any): any {
-        for (const extension of this.extensions) {
-            app.use(extension.path, extension.app);
-        }
-    }
-
-
+    abstract AddRoute(route: ExpressRoute): void;
 }
 
 export abstract class ExpressApplication extends ExpressRoutingAddon {
     protected app = express();
-    protected adaper: ExpressRouter[] = [];
     protected port: string | number;
 
     constructor() {
@@ -105,15 +90,10 @@ export abstract class ExpressApplication extends ExpressRoutingAddon {
     }
 
     abstract AddRoute(route: ExpressRoute): void;
-    abstract initializeMiddlewares(): void;
-    protected initializeAdapter(app: any): any {
-        for (const adapter of this.adaper) {
-            app.use(adapter.Path, adapter.App);
-        }
-        return app;
-    }
+    abstract initializeMiddleware(): void;
+
     public AddAdapter(adapter: ExpressRouter): void {
-        this.adaper.push(adapter);
+        this.app.use(adapter.Path, adapter.App);
     }
 
     main(): void {
