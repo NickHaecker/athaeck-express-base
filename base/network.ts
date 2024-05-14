aea51e441653321d5a13d377812808c77362ff19
Nick Philipp Häcker <56677688+NickHaecker@users.noreply.github.com>
2024-05-14 22:50:48 +0200

    edit

--- base/network.ts
+++ base/network.ts
@@ -1,26 +1,57 @@
-import os from "os"
-import config from "config"
+import os from "os";
+import config from "config";
 
-const expressConfig: any = config.get("express")
+const expressConfig: any = config.get("express");
+// export function GetLocalIP(): string {
+//     const ifaces: NodeJS.Dict<os.NetworkInterfaceInfo[]> = os.networkInterfaces();
+//     let localIP: string = "localhost";
+
+//     if (!expressConfig.useNetwork) {
+//         return localIP
+//     }
+
+//     Object.keys(ifaces).forEach((ifname) => {
+//         const networkInterface = ifaces[ifname]
+//         if (networkInterface === undefined) {
+//             return;
+//         }
+//         networkInterface.forEach((iface) => {
+//             if (iface.family === 'IPv4' && !iface.internal) {
+//                 localIP = iface.address;
+//             }
+//         });
+//     });
+
+//     return localIP;
 export function GetLocalIP(): string {
     const ifaces: NodeJS.Dict<os.NetworkInterfaceInfo[]> = os.networkInterfaces();
     let localIP: string = "localhost";
 
     if (!expressConfig.useNetwork) {
-        return localIP
+        return localIP;
     }
 
+    const desiredAdapter = "Ethernet";
+
     Object.keys(ifaces).forEach((ifname) => {
-        const networkInterface = ifaces[ifname]
+        console.log(ifname);
+        if (ifname !== desiredAdapter) {
+            return;
+        }
+
+        const networkInterface = ifaces[ifname];
         if (networkInterface === undefined) {
             return;
         }
+
         networkInterface.forEach((iface) => {
-            if (iface.family === 'IPv4' && !iface.internal) {
+            if (iface.family === "IPv4" && !iface.internal) {
                 localIP = iface.address;
             }
         });
     });
+
+    console.log(localIP);
 
     return localIP;
 }
